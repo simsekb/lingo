83684f1aa2a856bf02a7716f8cc91e08e427dcf9
Berkay <simsek.berkay@hotmail.com>
2020-12-31 15:35:32 +0100

    database change for heroku

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'), //mysql
+    'default' => env('DB_CONNECTION', 'pgsql'), //mysql
 
     /*
     |--------------------------------------------------------------------------
